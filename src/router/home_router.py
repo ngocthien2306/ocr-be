48560def96bdacc53513ca6daa7329e0d54c1386
thien-ds <thiennguyen@i-soft.com.vn>
2024-06-13 13:59:10 +0700

    update pages

--- src/router/home_router.py
+++ src/router/home_router.py
@@ -11,3 +11,35 @@ router = APIRouter(prefix="")
 @router.get("/")
 async def home_intr():
     return FileResponse("static/index.html")
+
+@router.get("/404")
+async def error():
+    return FileResponse("static/404.html")
+
+@router.get("/contact")
+async def contact():
+    return FileResponse("static/contact.html")
+
+@router.get("/faq")
+async def faq():
+    return FileResponse("static/faq.html")
+
+@router.get("/feature")
+async def feature():
+    return FileResponse("static/feature.html")
+
+@router.get("/project")
+async def project():
+    return FileResponse("static/project.html")
+
+@router.get("/service")
+async def service():
+    return FileResponse("static/service.html")
+
+@router.get("/team")
+async def team():
+    return FileResponse("static/team.html")
+
+@router.get("/testimonial")
+async def testimonial():
+    return FileResponse("static/testimonial.html")
